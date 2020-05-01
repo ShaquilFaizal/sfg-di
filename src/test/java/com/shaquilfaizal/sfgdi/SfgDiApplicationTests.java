d7d212aa40496c4d41e73a92b2dc143519be2f6c
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-05-01 17:03:47 +0530

    initial commit

---
+++ src/test/java/com/shaquilfaizal/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaquilfaizal.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
