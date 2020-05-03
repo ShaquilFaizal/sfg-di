bf97f36e3d847b134a87be9ca8e493994d26ef9e
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-05-03 16:24:39 +0530

    Add Controller

---
+++ src/main/java/com/shaquilfaizal/sfgdi/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.shaquilfaizal.sfgdi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+	public String sayHello() {
+		
+		System.out.println("Hello World");
+		
+		return "Hi Folks!";
+	} 
+}
